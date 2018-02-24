273283684d57efb6b081ce8d961cd59496f43531
zhujiajia <772978409@qq.com>
2018-02-24 14:14:00 +0800

    初始化less项目

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import less from './style/index.less';
+
+
+//
+ReactDOM.render(
+	<div className="container">Hello, world!</div>,
+	document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        port: 19231,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map', // source-map
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: { loader: 'babel-loader' }
+        }, {
+            test: /\.css$/,
+            use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+            ]
+        }, {
+            test: /\.less$/,
+            use: [
+                { loader: "style-loader" },
+                { loader: "css-loader" },
+                {
+                    loader: "less-loader",
+                    options: {
+                        modifyVars: {
+                            '@nice-blue': 'red'
+                        }
+                    }
+                },
+            ]
+        }, ]
+    }
+}
