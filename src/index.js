f80c75c7d147f39b4bb2213d587a28af9e3fe37d
zhujiajia <772978409@qq.com>
2018-02-27 09:55:03 +0800

    更新字体文件

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 // import { Button } from 'antd';
 // import 'antd/dist/antd.less'
 import './style/index.less';
+import './style/icon.less';
 import { Icon, Button } from 'antd'; // 加载 JS
 // import 'antd/lib/button/style'; // 加载 less
 
 const a1 = <div className="atc">Hello, world!</div>;
 const a2 = <div>
 				<Button type="primary">Primary</Button>
-				<Icon type="link" />
+				<Icon type="smile" />
+				<i className="iconfont icon-ticket"></i>
 			</div>;
 
 //
